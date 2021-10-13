32c434c4466774f427e425088b5de0fe36064e63
Karim Cheaibi <karimc77@hotmail.fr>
2021-10-13 15:15:18 +0200

    modification controllers et routes pour sauce.js, intègration like et dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -9,7 +9,11 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: []
   });
   sauce.save()
     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
@@ -51,3 +55,102 @@ exports.getAllSauces = (req, res, next) => {
       .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({ error }));
 };
+
+// Like ou Dislike Sauce
+// Pour la route READ = Ajout/suppression d'un like / dislike à une sauce
+// Like présent dans le body
+// On prend le userID
+// On prend l'id de la sauce
+exports.likeDislike = (req, res, next) => {
+  let like = req.body.like  
+  let userId = req.body.userId  
+  let sauceId = req.params.id
+
+  if (like === 1) { // S'il s'agit d'un like
+    Sauce.updateOne({
+        _id: sauceId
+      }, {
+        // On push l'utilisateur et on incrémente le compteur de 1
+        $push: {
+          usersLiked: userId
+        },
+        $inc: {
+          likes: +1
+        }, // On incrémente de 1
+      })
+      .then(() => res.status(200).json({
+        message: 'j\'aime ajouté !'
+      }))
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
+  if (like === -1) {
+    Sauce.updateOne( // S'il s'agit d'un dislike
+        {
+          _id: sauceId
+        }, {
+          $push: {
+            usersDisliked: userId
+          },
+          $inc: {
+            dislikes: +1
+          }, // On incrémente de 1
+        }
+      )
+      .then(() => {
+        res.status(200).json({
+          message: 'Dislike ajouté !'
+        })
+      })
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
+  if (like === 0) { // S'il s'agit d'annuler un like ou un dislike
+    Sauce.findOne({
+        _id: sauceId
+      })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(userId)) { // S'il s'agit d'annuler un like
+          Sauce.updateOne({
+              _id: sauceId
+            }, {
+              $pull: {
+                usersLiked: userId
+              },
+              $inc: {
+                likes: -1
+              }, // On incrémente de -1
+            })
+            .then(() => res.status(200).json({
+              message: 'Like retiré !'
+            }))
+            .catch((error) => res.status(400).json({
+              error
+            }))
+        }
+        if (sauce.usersDisliked.includes(userId)) { // S'il s'agit d'annuler un dislike
+          Sauce.updateOne({
+              _id: sauceId
+            }, {
+              $pull: {
+                usersDisliked: userId
+              },
+              $inc: {
+                dislikes: -1
+              }, // On incrémente de -1
+            })
+            .then(() => res.status(200).json({
+              message: 'Dislike retiré !'
+            }))
+            .catch((error) => res.status(400).json({
+              error
+            }))
+        }
+      })
+      .catch((error) => res.status(404).json({
+        error
+      }))
+  }
+}
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -13,16 +13,26 @@ const sauceCtrl = require('../controllers/sauce');
 // Dans le fichier du dossier routes, on va remplacer app.* par router.* et remplacer /api/sauces par /
 //Routes pour CRUD complet (creation, lecture, modification, suppression des ressources de l'application)
 
-// Route pour créer une sauce
-// Route pour modifier une sauce
-// Route pour supprimer une sauce
-// Route pour recuperer une sauce avec son id
-// Route pour recuperer toutes les sauces
+// 1. Route pour créer une sauce
+// 2. Route pour modifier une sauce
+// 3. Route pour supprimer une sauce
+// 4. Route pour recuperer une sauce avec son id
+// 5. Route pour recuperer toutes les sauces
+
+// 6. Route pour les likes et dislikes
+// Pour userID fourni:
+// Si j'aime = 1,l'utilisateur aime la sauce.
+// Si j'aime = 0,l'utilisateur annule ce qu'il aime ou ce qu'il n'aime pas.
+// Si j'aime =-1,l'utilisateur n'aime pas la sauce.
+// L'identifiant de l'utilisateur doit être ajouté ou supprimé du tableau approprié, en gardant une trace de ses préférences
+// et en l'empêchant d'aimer ou de ne pas aimer la même sauce plusieurs fois.
+// Nombre total de "j'aime" et de "je n'aime pas" à mettre à jour avec chaque "j'aime".
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, sauceCtrl.likeDislike);
 
 module.exports = router;
